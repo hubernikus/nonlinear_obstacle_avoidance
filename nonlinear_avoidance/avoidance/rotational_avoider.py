Starting to fix embedded-vector-rotation.

nonlinear_avoidance/avoidance/rotational_avoider.py
@@ -21,15 +21,14 @@ from vartools.directional_space import get_directional_weighted_sum
 from vartools.directional_space import get_angle_from_vector
 from vartools.directional_space import get_vector_from_angle
 from vartools.directional_space import UnitDirection
-
-# from vartools.directional_space DirectionBase
 from vartools.dynamical_systems import DynamicalSystem
 
 from dynamic_obstacle_avoidance.utils import compute_weights
 from dynamic_obstacle_avoidance.utils import get_weight_from_inv_of_gamma
 from dynamic_obstacle_avoidance.utils import get_relative_obstacle_velocity
-
 from dynamic_obstacle_avoidance.avoidance import BaseAvoider
+
+from nonlinear_avoidance.vector_rotation import VectorRotationTree
 
 # Type definition
 Vector = np.ndarray
@@ -232,11 +231,7 @@ class RotationalAvoider(BaseAvoider):
                 # base = DirectionBase(matrix=null_matrix)
                 base = null_matrix
 
-                # rotated_velocities[:, it] = UnitDirection(base).from_vector(initial_velocity)
                 rotated_directions[:, it] = initial_velocity
-                # rotated_directions[it] = UnitDirection(base).from_vector(
-                #     initial_velocity
-                # )
                 continue
 
             if hasattr(obstacle_list, "convergence_radiuses") and len(
@@ -254,13 +249,9 @@ class RotationalAvoider(BaseAvoider):
                 nonlinear_velocity=initial_velocity,
                 base=null_matrix,
                 convergence_radius=convergence_radius,
-                # base=DirectionBase(matrix=null_matrix),
             )
 
         base = get_orthogonal_basis(initial_velocity)
-        # rotated_velocity = get_directional_weighted_sum_from_unit_directions(
-        #     base=base, weights=weights, unit_directions=rotated_directions
-        # )
 
         rotated_velocity = get_directional_weighted_sum(
             null_direction=base[:, 0],
@@ -606,10 +597,6 @@ class RotationalAvoider(BaseAvoider):
             base_angle[0] = convergence_radius
             return UnitDirection(dir_reference.base).from_angle(base_angle)
 
-        # if dir_convergence.norm() > convergence_radius:
-        #     # If on the surface -> no need to 'project'
-        #     return dir_convergence
-
         surface_angle = get_intersection_with_circle(
             start_position=dir_reference.as_angle(),
             direction=(dir_convergence - dir_reference).as_angle(),
@@ -752,7 +739,7 @@ class RotationalAvoider(BaseAvoider):
                 weight = 0
             elif continuation_weight < 1:
                 continuation_weight = (
-                    continuation_weight ** self.smooth_continuation_power
+                    continuation_weight**self.smooth_continuation_power
                 )
                 weight = weight ** (1.0 / continuation_weight)
 
@@ -809,5 +796,20 @@ class RotationalAvoider(BaseAvoider):
                 (dir_convergence_tangent.as_vector(), dir_initial.as_vector())
             ).T,
         )
+
+        if do_graph_summing := False:
+            # Do the graph summing
+            direction_tree = VectorRotationTree()
+            direction_tree.set_root(root_idx=-1, direction=dir_convergence.as_vector())
+            direction_tree.add_node(
+                node_id=0, parent_id=-1, direction=dir_convergence_tangent.as_vector()
+            )
+            direction_tree.add_node(
+                node_id=1, parent_id=-1, direction=dir_initial.as_vector()
+            )
+
+            averaged_direction = direction_tree.get_weighted_mean(
+                node_list=[0, 1], weights=[weight, (1 - weight)]
+            )
 
         return rotated_velocity

nonlinear_avoidance/dynamics/segmented_dynamics.py
@@ -1,4 +1,3 @@
-import math
 from dataclasses import dataclass
 
 import numpy as np
@@ -26,8 +25,9 @@ class DynamicsSegment:
     end: np.ndarray
 
     # Defined in post-init
-    direction: np.ndarray = None
-    length: np.ndarray = None
+    # direction: np.ndarray = None
+    # length: np.ndarray = None
+    perpendicular_factor = 3.0
 
     def __post_init__(self):
         self.start = np.array(self.start)
@@ -54,7 +54,7 @@ class DynamicsSegment:
         projection = self.start + self.direction * tt
         dir_perp = projection - position
 
-        vector = dir_perp + self.direction
+        vector = self.perpendicular_factor * dir_perp + self.direction
         vector = vector / np.linalg.norm(vector)
         # return rotate(self.orientation, vector)
         return vector
@@ -110,7 +110,7 @@ class WavyPathFollowing(Dynamics):
     def dimension(self):
         return 2
 
-    def get_weights(self, position, weight_power=2, weight_factor=4):
+    def get_weights(self, position, weight_power=2, weight_factor=10):
         distances = np.zeros(self.n_segments)
 
         for ii, segment in enumerate(self.segments):

nonlinear_avoidance/nonlinear_rotation_avoider.py
@@ -44,6 +44,7 @@ from nonlinear_avoidance.rotation_container import RotationContainer
 from nonlinear_avoidance.dynamics.projected_rotation_dynamics import (
     ProjectedRotationDynamics,
 )
+from nonlinear_avoidance.vector_rotation import VectorRotationTree
 
 
 def get_convergence_weight(gamma: float) -> float:
@@ -109,7 +110,15 @@ class SingularityConvergenceDynamics(BaseAvoider):
             **kwargs,
         )
 
-        self.obstacle_convergence = obstacle_convergence
+        if obstacle_convergence is None:
+            # Assumption of stable center
+            self.obstacle_convergence = ProjectedRotationDynamics(
+                attractor_position=initial_dynamics.attractor_position,
+                initial_dynamics=initial_dynamics,
+            )
+
+        else:
+            self.obstacle_convergence = obstacle_convergence
 
     @property
     def dimension(self):
@@ -146,13 +155,17 @@ class SingularityConvergenceDynamics(BaseAvoider):
         local_convergence_velocity = self.evaluate_weighted_dynamics(
             position, initial_velocity
         )
-
-        return self._rotation_avoider.avoid(
+        rotated_velocity = self._rotation_avoider.avoid(
             position=position,
             initial_velocity=initial_velocity,
             convergence_velocity=local_convergence_velocity,
             **kwargs,
         )
+        # print("init vel", initial_velocity)
+        # print("conv vel", local_convergence_velocity)
+        # print("rota vel", rotated_velocity)
+
+        return rotated_velocity
 
     def get_base_convergence(self, position: np.ndarray) -> np.ndarray:
         # TODO: test this...
@@ -163,6 +176,9 @@ class SingularityConvergenceDynamics(BaseAvoider):
 
     def evaluate_convergence_around_obstacle(self, position, obstacle):
         raise NotImplementedError()
+
+    def compute_gamma_weights(self) -> np.ndarray:
+        return
 
     def evaluate_weighted_dynamics(
         self, position: np.ndarray, initial_velocity: np.ndarray
@@ -187,7 +203,7 @@ class SingularityConvergenceDynamics(BaseAvoider):
                 position, in_global_frame=True
             )
 
-        gamma_min = 1
+        gamma_min = 1.0
         # Store weights -> mostly for visualization
         self.weights = np.zeros(self.n_obstacles)
 
@@ -195,7 +211,7 @@ class SingularityConvergenceDynamics(BaseAvoider):
         if sum_close := np.sum(ind_obs):
             # Dangerously close..
             weights = np.ones(sum_close) * 1.0 / sum_close
-            weight_sum = 1
+            weight_sum = 1.0
 
         else:
             ind_obs = gamma_array < self._rotation_avoider.cut_off_gamma
@@ -235,6 +251,8 @@ class SingularityConvergenceDynamics(BaseAvoider):
 
         # Initial velocity will be the 'base velocity'
         local_velocities = np.zeros((self.dimension, np.sum(ind_obs)))
+        direction_tree = VectorRotationTree()
+        direction_tree.set_root(root_idx=-1, direction=initial_velocity)
 
         # Evaluating center directions for the relevant obstacles
         for ii, it_obs in enumerate(np.arange(self.n_obstacles)[ind_obs]):
@@ -249,6 +267,10 @@ class SingularityConvergenceDynamics(BaseAvoider):
                 position, obstacle=self._rotation_avoider.obstacle_environment[ii]
             )
 
+            direction_tree.add_node(
+                direction=local_velocities[:, ii], node_id=it_obs, parent_id=-1
+            )
+
             if not LA.norm(local_velocities[:, ii]):
                 # What should be done here (?)
                 # <-> smoothly reduce the weight as we approach the center(?)
@@ -261,6 +283,16 @@ class SingularityConvergenceDynamics(BaseAvoider):
             weights=weights,
             directions=local_velocities,
         )
+        tree_average_dir = direction_tree.get_weighted_mean(
+            node_list=np.arange(self.n_obstacles)[ind_obs],
+            weights=self.weights[ind_obs],
+        )
+
+        # TODO: finish this with history-of-rotation
+        # print("tree_average_dir", tree_average_dir)
+        # print("averaged_direction", averaged_direction)
+        # breakpoint()
+
         return initial_norm * averaged_direction
 
 

scripts/rotational/example_wavy_path_following.py
@@ -12,10 +12,13 @@ from nonlinear_avoidance.dynamics.segmented_dynamics import create_segment_from_
 
 
 def main(n_grid=30):
-    x_lim = [-1, 10]
-    y_lim = [-1, 10]
+    x_lim = [-5, 5]
+    y_lim = [-5, 5]
 
-    dynamics = create_segment_from_points([[0.0, 0], [4.0, 0], [4.0, 4.0], [8.0, 4.0]])
+    # dynamics = create_segment_from_points([[0.0, 0], [4.0, 0], [4.0, 4.0], [8.0, 4.0]])
+    dynamics = create_segment_from_points(
+        [[-4.0, -2.5], [0.0, -2.5], [0.0, 2.5], [4.0, 2.5]]
+    )
 
     # egments.append(DynamicsSegment(, [3.0, 0]))
     # segments.append(DynamicsSegment([4.0, 1], [4.0, 5]))
@@ -50,7 +53,7 @@ def main(n_grid=30):
     it_max = 1000
     dt = 0.1
     trajectory = np.zeros((dimension, it_max))
-    position_start = np.array([0.1, -0.4])
+    position_start = np.array([-4, -4.0])
     trajectory[:, 0] = position_start
     for ii in range(1, it_max):
         velocity = dynamics.evaluate(trajectory[:, ii - 1])