6bc4e82e2b9a81c8d288e15068d9a679e30aec57
Lukas Huber <hubernikus@gmail.com>
2023-05-05 15:55:44 +0200

    Create dynamics_sequence-wrapper.

---
+++ nonlinear_avoidance/dynamics/sequenced_dynamics.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from vartools.dynamics import Dynamics
+from nonlinear_avoidance.vector_rotation import VectorRotationSequence
+
+
+def evaluate_dynamics_sequence(
+    position: np.ndarray, dynamics: Dynamics
+) -> VectorRotationSequence:
+    if hasattr(dynamics, "evaluate_dynamics_sequence"):
+        return dynamics.evaluate_dynamics_sequence(position)
+
+    # Otherwise create based on direction towards stable-attractor
+    base = dynamics.attractor_position - position
+    final = dynamics.evaluate(position)
+
+    return VectorRotationSequence.create_from_vector_array(np.vstack((base, final)).T)
