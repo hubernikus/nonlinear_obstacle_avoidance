22735825b969b1d7ac4c9b3c9e6452e44ea07444
Lukas Huber <hubernikus@gmail.com>
2023-05-09 17:39:03 +0200

    Separate qolo-plotting.

---
+++ nonlinear_avoidance/visualization/plot_qolo.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import numpy as np
+
+from scipy import ndimage
+import matplotlib.pyplot as plt
+
+
+def plot_qolo(position, direction, ax):
+    arr_img = plt.imread(Path("media", "Qolo_T_CB_top_bumper.png"))
+
+    length_x = 1.2
+    length_y = (1.0) * arr_img.shape[0] / arr_img.shape[1] * length_x
+
+    rot = np.arctan2(direction[1], direction[0])
+    arr_img_rotated = ndimage.rotate(arr_img, rot * 180.0 / np.pi, cval=255)
+
+    length_x_rotated = np.abs(np.cos(rot)) * length_x + np.abs(np.sin(rot)) * length_y
+
+    length_y_rotated = np.abs(np.sin(rot)) * length_x + np.abs(np.cos(rot)) * length_y
+
+    ax.imshow(
+        arr_img_rotated,
+        extent=[
+            position[0] - length_x_rotated / 2.0,
+            position[0] + length_x_rotated / 2.0,
+            position[1] - length_y_rotated / 2.0,
+            position[1] + length_y_rotated / 2.0,
+        ],
+        zorder=1,
+    )
+
+
+def integrate_with_qolo(start_position, velocity_functor, it_max=70, dt=0.1, ax=None):
+    dimension = 2
+    positions = np.zeros((dimension, it_max + 1))
+    positions[:, 0] = start_position
+
+    for pp in range(it_max):
+        velocity = velocity_functor(positions[:, pp])
+        positions[:, pp + 1] = positions[:, pp] + velocity * dt
+
+    if ax is not None:
+        # Plot trajectory & Qolo
+        ax.plot(
+            positions[0, :], positions[1, :], color="#DB4914", linewidth=4.0, zorder=-2
+        )
+        # ax.plot(positions[0, :], positions[1, :], ".", linewidth=2.0)
+        ax.plot(positions[0, 0], positions[1, 0], marker="o", color="k")
+        plot_qolo(ax=ax, position=positions[:, -1], direction=velocity)
+
+    return positions
